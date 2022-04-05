ee7a5b441c0d46b696989f05c77989747831e3ce
Enric Meinhardt-Llopis <enric.meinhardt@cmla.ens-cachan.fr>
2022-04-05 12:51:49 +0200

    incidence matrix

--- ipwg/a.py
+++ ipwg/a.py
@@ -1,3 +1,45 @@
+# # Image processing with graphs
+
+# A graph is a sparse matrix.  Thus, libraries for doing linear algebra are the
+# appropriate tool to deal with graphs.  In this notebook we show how to
+# implement some common operations in image processing using a graph formalism.
+# In this formalism, images are functions that assign a number to each vertex
+# of a grid graph.  Here the graph will always be a grid, but all the
+# operations can be defined over an arbitrary graph.
+#
+# This version of the notebook uses python/numpy.  There are other equivalent
+# versions with octave and julia.
+
+# ## Construction of the grid graph
+
+# An image of size $W\times H$ has $n=WH$ pixels.  These are the vertices
+# of our grid graph.  The edges connect the—up to four—neighbors of each pixel.
+# Thus there are $H$ rows of $W-1$ horizontal edges each, and $W$ columns of
+# $H-1$ vertical edges, for a total of $m$ edges
+
+# $$m=H(W-1)+W(H-1)$$
+
+# The following function builds the signed incidence matrix of a $W\times H$
+# grid graph.
+
+def incidence_grid(w, h):
+	""" Build the signed incidence matrix of a WxH grid graph """
+	from scipy.sparse import eye, kron, vstack
+	x = eye(w-1, w, 1) - eye(w-1, w)             # path graph of length W
+	y = eye(h-1, h, 1) - eye(h-1, h)             # path graph of length H
+	Bx = kron(eye(h), x)                         # H horizontal paths
+	By = kron(y, eye(w))                         # W vertical paths
+	B = vstack([Bx, By])                         # union of all paths
+	return B
+
+B = incidence_grid(2,3)
+
+# ## Morphological operators
+
+# ## Local linear operators
+
+# ## Linear differential equations
+
 # ---
 # format:
 #     html:
@@ -8,25 +50,12 @@
 #     text_representation:
 #       extension: .py
 #       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.13.7
 #   kernelspec:
 #     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
 
-# # Title of the notebook
-
-import iio
-from IPython.display import display,Image
-
-x = iio.read("tut/f/barbara.png")
-
-x.shape
-
-# Some math $\int_\Omega d\omega=\int_{\partial\Omega}\omega$
-
-display(Image("tut/f/barbara.png"))
 
 
+# vim:set tw=79 filetype=python spell spelllang=en:

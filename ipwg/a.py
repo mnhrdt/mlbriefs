fd31631c74fdf41078d955470ba9480849bfc871
Enric Meinhardt-Llopis <enric.meinhardt@ens-paris-saclay.fr>
2022-04-06 17:03:54 +0200

    further article and some hacks

--- ipwg/a.py
+++ ipwg/a.py
@@ -22,7 +22,7 @@
 # The following function builds the signed *incidence matrix* of a $W\times H$
 # grid graph.  It is a matrix of $n$ columns and $m$ rows.
 
-def grid_incidence(w, h):
+def grid_incidence(h, w):
 	""" Build the signed incidence matrix of a WxH grid graph """
 	from scipy.sparse import eye, kron, vstack
 	x = eye(w-1, w, 1) - eye(w-1, w)             # path graph of length W
@@ -35,7 +35,7 @@ def grid_incidence(w, h):
 # A similar construction builds the *adjacency matrix*, which is square of size
 # $n$.
 
-def grid_adjacency(w, h):
+def grid_adjacency(h, w):
 	""" Build the adjacency matrix of a WxH grid graph """
 	from scipy.sparse import eye, kronsum
 	x = eye(w, w, 1)                             # oriented path of length W
@@ -46,13 +46,100 @@ def grid_adjacency(w, h):
 
 # We will also construct the *Laplacian matrix* of the graph, defined as
 # $L=-B^\top B$.  There are many algebraic relationships between the Laplacian,
-# adjacency and incidence matrix.  The non-diagonal entries of the Laplacian
-# matrix are the adjacency matrix.
+# adjacency and incidence matrices.
+# The matrices $A$ and $L$ are square, each row of these matrices
+# corresponding to one pixel of the original image domain.  Since these
+# matrices are circulant, we can represent them by their effect on an isolated
+# pixel; this is called the *stencil* of the linear operator associated to the
+# matrix (or the kernel of the corresponding convolution operator):
+
+# $$
+# \mathrm{stencil}(A)=
+# \begin{array}{|c|c|c|}\hline0&1&0\\\hline1&0&1\\\hline0&1&0\\\hline\end{array}
+# 	\qquad
+# 	\qquad
+# \mathrm{stencil}(L)=
+# \begin{array}{|c|c|c|}\hline0&1&0\\\hline1&\!\!\!-4\!\!\!&1\\\hline0&1&0\\\hline\end{array}
+# $$
+
+# We also define a few administrative functions to load and display
+# images:
+
+def image_read(f):
+	""" Read an image from filename "f" into a numpy array """
+	import iio
+	x = iio.read(f).squeeze()
+	return x
+
+def image_write(f, x):
+	""" Write the numpy array "x" into an image file "f" """
+	import iio
+	iio.write(f, x)
+	return
+
+def image_display(f):
+	""" Inline display the image from filename "f" into a numpy array """
+	import IPython.display
+	s = f"<div><p><img src=\"{f}\" /> {f}</p></div>"
+	IPython.display.display(IPython.display.HTML(s))
+	return
+
+def image_render(f, x):
+	""" Write the numpy array "x" into an image file "f" and display it """
+	image_write(f, x)
+	image_display(f)
+	return
+
+# Let's try these functions with a trivial example:
+
+x = image_read("x.png")            # load an image from current directory
+h, w = x.shape                      # extract height and width (note the order)
+x[:,:w//2] = 0                      # paint the left side in black
+x = image_write("x_cut.png", x)    # save the modified image
+image_display("x.png")             # display original image
+image_display("x_cut.png")         # display modified image
+
 
 # ## Morphological operators
 
 # Mathematical morphology is based on applying the adjacency matrix as an
 # operator acting on binary images.
+# The neighborhood of each pixel, as defined by the graph, plays the role of the
+# *structuring element*.
+
+# First, we read the image data $x$ to use through the rest of this
+# section, and build its adjacency matrix $A$.
+
+x = image_read("x.png")
+h, w = x.shape
+x = x.flatten()
+A = grid_adjacency(h, w)
+
+# Now, let's be silly: what happens when we apply $A$ to $x$ ?
+
+y = A @ x
+image_render("x_A.png", y.reshape(h, w))
+
+# The result is a bit difficult to interpret... the image looks much brighter
+# and maybe a bit blurry.
+
+# Now let's do some morphology
+
+m = 1.0 * (x > 66)          # binarized image
+E = A.copy();E.setdiag(1)   # structuring element E = A + I
+y = E**6 @ m                # apply it 6 times
+y1 = y > 0                  # extract dilation
+y2 = 1 - (y < y.max())      # extract erosion
+y3 = y > y.max()/2          # extract median filter
+
+# save the results
+
+image_render("x_binarized.png",    255 * m.reshape(h, w))
+image_render("x_6_morphogray.png", 255 * y.reshape(h, w) / y.max())
+image_render("x_6_dilation.png",   255 * y1.reshape(h, w))
+image_render("x_6_erosion.png",    255 * y2.reshape(h, w))
+image_render("x_6_median.png",     255 * y3.reshape(h, w))
+
 
 # ## Local linear operators
 
@@ -62,6 +149,7 @@ def grid_adjacency(w, h):
 # format:
 #     html:
 #         self-contained: true
+#         preserve-tabs: true
 # jupyter:
 #   jupytext:
 #     formats: py:light,qmd
